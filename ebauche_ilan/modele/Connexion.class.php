09a2acc74e80df4bc8b3465356f89f42d36f810f
fosfor12345 <enkalus@gmail.com>
2016-12-09 14:00:22 +0100

    er

--- ebauche_ilan/modele/Connexion.class.php
+++ ebauche_ilan/modele/Connexion.class.php
@@ -4,7 +4,7 @@ Class Connexion{
 	public function getDatabase(){
 		try {
 			$bdd = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE; charset=utf8', 'root', 'root');
-			echo "Connexion etablie avec la bade de donnÃ©e";
+			echo "Connexion etablie avec la bade de donnée";
 		} catch (PDOException $e) {
 			die('Erreur : ' . $e->getMessage());
 		}
