9d6cc7766a1e76233927fdb901c4a0932ebd2dd7
Ilan <fosfor12345@hotmail.fr>
2016-12-12 15:37:49 +0100

    Modification de About

--- ebauche_ilan/Vue/about.php
+++ ebauche_ilan/Vue/about.php
@@ -15,8 +15,7 @@
 			
 			  <div class="content embossed">
 			    <p class="embossedtext">
-			Lorem ipsum dolor sit amet, consect adipiscing elit. Nulla placerat, arcuf sit amet interdum malesuada, nunc et nibh porta augue, non tincidunt au i metus quis neque.</br></br>Praesent sapien diam, interdum quiste pretium quis, blandit et velit. Donec in nisi quis erat varius convallis nec a felis. Integer condimentum dolor a.</p>
-			<p class="embossedtext icons"><i class="fa fa-twitter" aria-hidden="true"></i><i class="fa fa-facebook" aria-hidden="true"></i><i class="fa fa-pinterest-p" aria-hidden="true"></i></p>
+			    Fait par le grand Gourou
 			  </div>
 </div>
 		
