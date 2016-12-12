a371e6ed733bda030f1b1d2e40834e078f37e6ab
Ilan <fosfor12345@hotmail.fr>
2016-12-12 13:35:48 +0100

    Comment

--- ebauche_ilan/modele/Connexion_Base.class.php
+++ ebauche_ilan/modele/Connexion_Base.class.php
@@ -1,11 +1,11 @@
 <?php
 class Connexion_Base{
 	private $db;
-	
+	//Constructeur
 	public function __construct(){
 		$this->db = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE;charset=utf8', 'root', 'root');
 	}
-	
+	//Getter 
 	public function getDb(){
 		return $this->db;
 	}
