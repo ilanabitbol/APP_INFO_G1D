7eafc6f07d3662d44449f0e2d554ae5a1d8742ee
rhaennig <rhaennig@orange.fr>
2016-12-13 17:42:21 +0100

    changement des commantaire avec accents

--- ebauche_ilan/controleur/deconnexion_controleur.php
+++ ebauche_ilan/controleur/deconnexion_controleur.php
@@ -4,10 +4,10 @@
 // On appelle la session
 session_start();
 
-// On �crase le tableau de session
+// On ecrase le tableau de session
 $_SESSION = array();
 
-// On d�truit la session
+// On detruit la session
 session_destroy();
 
 header("Location: ../Vue/sign_in-up.php");
