60b3628ca79fd2cb681390956414d3f73fffd36c
Alchemy3 <p.guezennec@gmail.com>
2016-12-13 10:43:40 +0100

    Add files via upload

---
+++ ebauche_ilan/entete.php
@@ -0,0 +1,30 @@
+<?php
+	print_r($_SESSION);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Dosmoz form</title>
+	<link rel="stylesheet" type="text/css" href="../stylesheet/entete.css">
+</head>
+<body>
+		
+		<nav id="nav-2">
+		  <a class="link-2" href="dashboard.php">Dashboard</a>
+		  <a class="link-2" href="about.php">About</a>
+		  <a class="link-2" href="contact.php">Contact</a>
+		  <a class="link-2" href="#">Shop</a>
+		  <a class="link-2" href="sign_in-up.php">Connexion-Inscription</a>
+		  <a class="link-2">
+		  <?php 
+		  session_start();
+		  echo 'Bienvenue boubou';
+		  echo  $_SESSION['email'];
+		  ?></a>
+		  <a class="link-2" href="../controleur/deconnexion_controleur.php">Déconnexion</a>
+		</nav>
+
+</body>
+
+</html>
