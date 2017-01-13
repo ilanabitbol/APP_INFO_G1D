db7e3719fdc8b9ef9dfcd62fc02c89b739fb8e8c
Pierre <Pierre@PC_Pierre>
2017-01-13 01:16:18 +0100

    MAJ mise en page shop

--- ebauche_ilan/Vue/shop.php
+++ ebauche_ilan/Vue/shop.php
@@ -7,54 +7,59 @@
 	<link rel="stylesheet" type="text/css" href="../stylesheet/shop.css">
 </head>
 <body>
-<?php
-		include_once ('../modele/Connexion_Base.class.php');
-		$connexion_base= new Connexion_Base();
-		// lecture dans la table catalogue
-		$reponse= $connexion_base->getDb()->query('SELECT nom_produit, prix, stock FROM catalogue');?>
-		
-		<?php while($donnes = $reponse->fetch()){?>
-		<div id="container">
-			<ul class="list">
-		        <li><?php 
-					switch ($donnes['nom_produit'])
-					{
-						case 'hydrometre':?>
-				  		<img alt="hydrometre" src="../images/rain.png"/>
-				  		<?php break;?>
-				<?php 
-						case 'thermometre':?>
-						<img alt="thermometre" src="../images/temperature.png"/>
-						<?php break;?>
-				<?php 
-						case 'pressiometre':?>
-						<img alt="pressiometre" src="../images/meter.png">
-						<?php break;?>
-						
-				<?php }?>
-				<form action="../Vue/paiement.php">
-    				<input type="submit" value="Acheter" class="buttons">
-				</form>
-		        
-		        
-		        
-		        <?php 
-		        		  echo 'Produit : '.$donnes['nom_produit'].'<br />';
-						  echo 'Prix : '.$donnes['prix'].' euros TTC'.'<br />';
-						  echo 'Stock : '.$donnes['stock'].'<br />'; 
-					?>
+	<div class="container_global">
+		<?php
+				include_once ('../modele/Connexion_Base.class.php');
+				$connexion_base= new Connexion_Base();
+				// lecture dans la table catalogue
+				$reponse= $connexion_base->getDb()->query('SELECT nom_produit, prix, stock FROM catalogue');?>
 				
-				
-					
-				</li>
-					
-    		</ul>
-		</div>
-		<?php 
-		}
-		$reponse->closeCursor(); // Termine le traitement de la requête
-?>
-	  
+				<?php while($donnes = $reponse->fetch()){?>
+				<div id="container">
+					<ul class="list">
+				        <li>
+					        <div class="innercontainer">
+						        <div id="content">
+						        	<section id="image">
+							        	<?php 
+											switch ($donnes['nom_produit'])
+											{
+												case 'hydrometre':?>
+										  		<img alt="hydrometre" src="../images/rain.png"/>
+										  		<?php break;?>
+										<?php 
+												case 'thermometre':?>
+												<img alt="thermometre" src="../images/temperature.png"/>
+												<?php break;?>
+										<?php 
+												case 'pressiometre':?>
+												<img alt="pressiometre" src="../images/meter.png">
+												<?php break;?>
+												
+										<?php }?>
+									</section>
+									<section id="infos">
+							        <?php 
+							        		  echo 'Produit : '.$donnes['nom_produit'].'<br />';
+											  echo 'Prix : '.$donnes['prix'].' euros TTC'.'<br />';
+											  echo 'Stock : '.$donnes['stock'].'<br />'; 
+										?>
+									</section>
+								</div>		
+								<form action="../Vue/paiement.php">
+						    			<input type="submit" value="Acheter" class="buttons">
+								</form>
+													
+							</div>
+						</li>
+							
+		    		</ul>
+				</div>
+				<?php 
+				}
+				$reponse->closeCursor(); // Termine le traitement de la requête
+		?>
+	</div>	  
 </body>
 
 
