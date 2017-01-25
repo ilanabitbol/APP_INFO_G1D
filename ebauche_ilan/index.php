52191aafd03556ba1260eb51a63341a96649fe30
Ilan <fosfor12345@hotmail.fr>
2017-01-25 23:18:51 +0100

    MAJ index

---
+++ ebauche_ilan/index.php
@@ -0,0 +1,24 @@
+<?php include 'gabarit.php' ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Dosmoz form</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet/about.css">
+</head>
+<body>
+	
+		<div class="container">
+			  <div class="circle embossed">
+			    <p class="embossedtext">Dosmoz</p>
+			  </div>
+			
+			  <div class="content embossed">
+			    <p class="embossedtext">
+			    Dosmoz est un projet r&Eacutealis&Eacute par une des &Eacutequipes de Mohai &Agrave la demande de DomIsep. Une &Eacutequipe de 6 grands apprentis ing&Eacutenieurs a con&ccedilu ce produit.
+			  </div>
+</div>
+		
+</body>
+
+</html>
