398364624acd9047b542aaa315e3f1f68b3ca46b
cradolanirina <clement.radolanirina@gmail.com>
2017-01-27 14:12:54 +0100

    mise en place d'envoi d'un mail

---
+++ ebauche_ilan/Vue/footer.php
@@ -0,0 +1,89 @@
+<footer class="footer" style="text-align:center;">
+<link rel="stylesheet" type="text/css" href="../stylesheet/footer.css">
+<nav id="nav-2">
+	<div id="footer_2">
+  
+	  	<div class="footer" align="left">
+	  		<h2> Nous contacter</h2>
+	  		<ul id="nav1"><!-- permet de supprimer les espaces blanc que l'on pourrait appercevoir
+	  		 --><li><a href="adresse http du faq" class="footerlist_link">FAQ</a></li><!--
+	     	 --><li><a href="envoi_mail.php" class="footerlist_link">Par e-mail</a></li><!--
+	     	 --><li><a href="contact.php" class="footerlist_link">Adresse postale</a></li>
+	  		</ul>
+	  		<!-- Mettre les bons liens,href!! Les liens seront ÃƒÂ  remplir par domisep -->
+	  		<h2> L'entreprise</h2>
+	  		<ul id="nav2">
+	     		<li><a href="" class="footerlist_link">Recrutement</a></li>
+	      		<li><a href="descritpion de l'entreprise domisep" class="footerlist_link">SociÃ©tÃ©</a></li>
+	      		<li><a href="" class="footerlist_link">Mentions lÃ©gales</a></li>
+	      	</ul>
+	  		<!-- A voir avec le client quels produits decrire pour attirer le client ... tous les produits ne sont pas a decrire -->
+	  		<h2> Nos produits</h2>
+	  		<ul id="nav3">
+	            <li><a href="description du produit 1" class="footerlist_link">Produit 1</a></li>
+	     		<li><a href="description du produit 2" class="footerlist_link">Produit 2</a></li>
+	      		<li><a href="description du produit 3" class="footerlist_link">Produit 3</a></li>
+	      		<li><a href="description du produit 4" class="footerlist_link">Produit 4</a></li>
+	      	</ul>
+	  	</div>  
+    
+    
+    
+	    <div class="media">
+			<h2 class="linksList__title footer__title foldable__button  js-foldable-button">Suivez-nous<i class="icon-next  foldable__icon"></i></h2>
+				<ul class="footer_socialNetwork">
+					<li>
+					<a href="https://www.facebook.com" target="_blank" class="fb-button">
+					<span class="hidden">Facebook</span>
+					<i class="fb-icon"></i>
+					</a>
+					</li>
+					
+					<li>
+					<a href="https://twitter.com" target="_blank" class="twitter-button">
+					<span class="hidden">Twitter</span>
+					<i class="twitter-icon"></i>
+					</a>
+					</li>
+					
+					<li>
+					<a href="https://instagram.com" target="_blank" class="insta-button">
+					<span class="hidden">Instagram</span>
+					<i class="insta-icon"></i>
+					</a>
+					</li>
+					
+					<li>
+					<a href="https://www.youtube.com" target="_blank" class="youtube-button">
+					<span class="hidden">Youtube</span>
+					<i class="youtube-icon"></i>
+					</a>
+					</li>
+					
+					<li>
+					<a href="https://www.linkedin.com" target="_blank" class="linkedin-button">
+					<span class="hidden">Linkedin</span>
+					<i class="linkedin-icon"></i>
+					</a>
+					</li>
+				</ul>
+		</div>
+    
+    
+    <!-- Liens vers le site avec langue europeenne !! traduction du site a faire par domisep -->
+    	<div class="ligne_bas">     
+          	<ul class="langue"> 
+			   <li><a href="adresse http du site en fr" class="footerlist_link">FranÃ§ais</a></li> 
+			   <li><a href="adresse http du site en de" class="footerlist_link">Deutsch</a></li> 
+			   <li><a href="adresse http du site en en" class="footerlist_link">English</a></li> 
+			   <li><a href="adresse http du site en russe" class="footerlist_link">PÃ‘Æ’Ã‘ï¿½Ã‘ï¿½Ã�ÂºÃ�Â¸Ã�Â¹</a></li>
+			   <li><a href="adresse http du site en es" class="footerlist_link">EspaÃƒÂ±ol</a></li>
+			   <li><a href="adresse http du site en it" class="footerlist_link">Italiano</a></li>
+			 </ul>
+     	</div>   
+	</div> 
+</nav>     
+</footer>
+</body>
+
+</html>
