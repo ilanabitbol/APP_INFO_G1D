69114729c95389c90ef2ba3b64f42a5c0b10dd0c
cradolanirina <clement.radolanirina@gmail.com>
2017-01-27 20:35:13 +0100

    suite mail 3

--- ebauche_ilan/modele/Connexion_Base.class.php
+++ ebauche_ilan/modele/Connexion_Base.class.php
@@ -3,7 +3,7 @@ class Connexion_Base{
 	private $db;
 	//Constructeur
 	public function __construct(){
-		$this->db = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE;charset=utf8', 'root', 'root');
+		$this->db = new PDO('mysql:host=sql101.byethost18.com;dbname=b18_19502826_app_g1d_base;charset=utf8', 'b18_19502826', 'P*2C*2IR*');
 	}
 	//Getter 
 	public function getDb(){
