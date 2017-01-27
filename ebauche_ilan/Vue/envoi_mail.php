afcaca888beb4d80baa228935ecf0e25b319b3dc
cradolanirina <clement.radolanirina@gmail.com>
2017-01-27 20:12:29 +0100

    suite mail 2

--- ebauche_ilan/Vue/envoi_mail.php
+++ ebauche_ilan/Vue/envoi_mail.php
@@ -49,6 +49,8 @@ session_start();
 				  <a href="https://www.google.fr/maps/place/ISEP/@48.824529,2.2798536,15z/data=!4m5!3m4!1s0x0:0xe0d3eb2ad501cb27!8m2!3d48.824529!4d2.2798536" class="footerlist_link" target="_blank">10 Rue de Vanves, Issy-les-Moulineaux,France</a><br>
 				</address>	
 </div>
+</body>
+</html>
 <?php
 unset($_SESSION['success']);
 unset($_SESSION['inputs']);
