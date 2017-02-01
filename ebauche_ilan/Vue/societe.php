d8071d8896f92e40c890c8842064f0fe6ac3321d
cradolanirina <clement.radolanirina@gmail.com>
2017-02-02 00:11:26 +0100

    page de description de DOMISEP + modification faites sur le footer

---
+++ ebauche_ilan/Vue/societe.php
@@ -0,0 +1,29 @@
+<?php include 'entete.php' ;?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Dosmoz form</title>
+	<link rel="stylesheet" type="text/css" href="../stylesheet/societe.css">
+</head>
+<body>
+		<div class="container">
+			  <div class="circle embossed">
+			    <p class="embossedtext">Dosmoz</p>
+			  </div>			
+			  <div class="content embossed">
+			    	<p class="embossedtext">
+DOMISEP est une grande soci&#233t&#233 qui poss&#232de des ing&#233nieurs g&#233n&#233ralistes dans tous les domaines du num&#233rique.<br><br>
+Elle est depuis 60 ans, fourni &#224 ses clients des prduits avec une v&#233ritable qualit&#233 et permet d'acc&#233der &#224 nos consommations afin d'&#234tre encore plus &#233cologique.<br><br>
+Ses activit&#233s sont tr&#232s vari&#233es (scientifiques, techniques, commerciales ou manag&#233riales) et dans tous les secteurs d'activit&#233s, en France comme &#224 l'&#233tranger (A&#233ronautique,
+  Automobile, T&#233l&#233communication, Sant&#233, Finance, Conseil, Spatial, etc.)<br><br><br>
+Gr&#226ce &#224 sa forte dimension internationale, elle a su &#233toffer ses effectifs et ainsi avoir la renom&#233e qu'elle a aujourd'hui.
+
+ DOMISEP, constamment class&#233e en t&#234te du fameux Customers Electronics Show  est tr&#232s largement pl&#233biscit&#233e 
+  par les m&#233 qui reconnaissent le talents des &#233quipes de tr&#232s haut niveau d'ing&#233nieurs que poss&#232 la soci&#233t&#233.
+  					</p>			  					
+  			  </div>
+</div>
+<footer><?php include 'footer.php';?></footer>
+</body>
+</html>
