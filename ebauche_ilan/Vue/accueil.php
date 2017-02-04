d31dd96352186179d8c9a97d2ddcdc53747d9df3
Ilan <fosfor12345@hotmail.fr>
2017-02-04 15:59:03 +0100

    evol accueil

--- ebauche_ilan/Vue/accueil.php
+++ ebauche_ilan/Vue/accueil.php
@@ -5,6 +5,8 @@
 	<meta charset="UTF-8">
 	<title>Dosmoz form</title>
 	<link rel="stylesheet" type="text/css" href="../stylesheet/accueil.css">
+		<link rel="stylesheet" type="text/css" href="../stylesheet/societe.css">
+	
 </head>
 <body>
 	
@@ -12,15 +14,52 @@
 	  
 	  		<ul class="wrapper">
 	    
-			    <li class="top_3px"><section class="produit">Thermostat</section></li>
-			    <li class="top_3px"><section class="produit">An&#233mom&#232tre</section></li>
-			    <li class="top_3px"><section class="produit">Cam&#233ra</section></li>
-			    <li class="top_3px"><section class="produit">Pluviom&#232tre</section></li>
-			    <li class="top_3px"><section class="produit">Station m&#233t&#233o</section></li>
+			    <li class="top_3px"><section class="produit">Thermostat</section>
+			   			 <section class="achetez">
+			   			 	<a  href='shop.php'>Achetez</a>	
+			   			 </section>
+			    </li>
+			    <li class="top_3px"><section class="produit">An&#233mom&#232tre</section>
+			    		<section class="achetez">
+			   			 	<a  href='shop.php'>Achetez</a>	
+			   			 </section>
+			    </li>
+			    <li class="top_3px"><section class="produit">Cam&#233ra</section>
+			    		<section class="achetez">
+			   			 	<a  href='shop.php'>Achetez</a>	
+			   			 </section>
+			    </li>
+			    <li class="top_3px"><section class="produit">Pluviom&#232tre</section>
+			    		<section class="achetez">
+			   			 	<a  href='shop.php'>Achetez</a>	
+			   			 </section>
+			    </li>
+			    <li class="top_3px"><section class="produit">Station m&#233t&#233o</section>
+			    		<section class="achetez">
+			   			 	<a  href='shop.php'>Achetez</a>	
+			   			 </section>
+			    </li>
 	    
 	 		 </ul>
 
 		</div>
+				<div class="container">
+			  <div class="circle embossed">
+			    <p class="embossedtext">Dosmoz</p>
+			  </div>
+		<div class="content embossed">
+			    	<p class="embossedtext">
+DOMISEP est une grande soci&#233t&#233 qui poss&#232de des ing&#233nieurs g&#233n&#233ralistes dans tous les domaines du num&#233rique.<br><br>
+Elle est depuis 60 ans, fourni &#224 ses clients des prduits avec une v&#233ritable qualit&#233 et permet d'acc&#233der &#224 nos consommations afin d'&#234tre encore plus &#233cologique.<br><br>
+Ses activit&#233s sont tr&#232s vari&#233es (scientifiques, techniques, commerciales ou manag&#233riales) et dans tous les secteurs d'activit&#233s, en France comme &#224 l'&#233tranger (A&#233ronautique,
+  Automobile, T&#233l&#233communication, Sant&#233, Finance, Conseil, Spatial, etc.)<br><br><br>
+Gr&#226ce &#224 sa forte dimension internationale, elle a su &#233toffer ses effectifs et ainsi avoir la renom&#233e qu'elle a aujourd'hui.
+
+ DOMISEP, constamment class&#233e en t&#234te du fameux Customers Electronics Show  est tr&#232s largement pl&#233biscit&#233e 
+  par les m&#233 qui reconnaissent le talents des &#233quipes de tr&#232s haut niveau d'ing&#233nieurs que poss&#232 la soci&#233t&#233.
+  					</p>			  					
+  			  </div>
+</div>
 		
 		 	<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
 			<script type="text/javascript" src="../stylesheet/accueil.js"></script>
