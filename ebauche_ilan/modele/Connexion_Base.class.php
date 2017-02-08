b7d458d19f699ed3e9ce9d817da1226a242ad08b
cradolanirina <clement.radolanirina@gmail.com>
2017-02-09 00:55:52 +0100

    test connexion base de donnee

--- ebauche_ilan/modele/Connexion_Base.class.php
+++ ebauche_ilan/modele/Connexion_Base.class.php
@@ -3,7 +3,7 @@ class Connexion_Base{
 	private $db;
 	//Constructeur
 	public function __construct(){
-		$this->db = new PDO('mysql:host=server102.hostinger.fr;dbname=APP_G1D_BASE;charset=utf8', 'u691514939', 'Cr19952002.');
+		$this->db = new PDO('mysql:host=mysql.hostinger.fr;dbname=u691514939_abyve;charset=utf8', 'u691514939_eqyde', 'Cr19952002.');
 		//$this->db = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE;charset=utf8', 'root', 'root');
 	}
 	//Getter 
