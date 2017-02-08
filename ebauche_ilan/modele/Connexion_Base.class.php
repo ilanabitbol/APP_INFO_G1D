5a5480d9888c2eb7eeefaf22f26a618d895a814b
cradolanirina <clement.radolanirina@gmail.com>
2017-02-09 00:39:05 +0100

    test clem pour hebergement du site

--- ebauche_ilan/modele/Connexion_Base.class.php
+++ ebauche_ilan/modele/Connexion_Base.class.php
@@ -3,7 +3,8 @@ class Connexion_Base{
 	private $db;
 	//Constructeur
 	public function __construct(){
-		$this->db = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE;charset=utf8', 'root', 'root');
+		$this->db = new PDO('mysql:host=server102.hostinger.fr;dbname=APP_G1D_BASE;charset=utf8', 'u691514939', 'Cr19952002.');
+		//$this->db = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE;charset=utf8', 'root', 'root');
 	}
 	//Getter 
 	public function getDb(){
