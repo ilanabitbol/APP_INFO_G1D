5cfd07853b236fafac23a91c886dc22c61046237
cradolanirina <clement.radolanirina@gmail.com>
2017-02-09 01:01:14 +0100

    test 2

--- ebauche_ilan/modele/Connexion_Base.class.php
+++ ebauche_ilan/modele/Connexion_Base.class.php
@@ -3,7 +3,7 @@ class Connexion_Base{
 	private $db;
 	//Constructeur
 	public function __construct(){
-		$this->db = new PDO('mysql:host=mysql.hostinger.fr;dbname=u691514939_abyve;charset=utf8', 'u691514939_eqyde', 'P*2C*2IR*');
+		$this->db = new PDO('mysql:host=mysql.hostinger.fr;dbname=u691514939_abyve;charset=utf8', 'u691514939_eqyde', 'Cr19952002.');
 		//$this->db = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE;charset=utf8', 'root', 'root');
 	}
 	//Getter 
