04da4823355bd8421d3dc6d128924d396e0f0c45
cradolanirina <clement.radolanirina@gmail.com>
2017-02-09 20:02:13 +0100

    retour de l'index normal

--- ebauche_ilan/index.php
+++ ebauche_ilan/index.php
@@ -1,5 +1,24 @@
-<?php include '../Vue/entete.php' ?>
-<?php include '../Vue/about.php'?>
+<?php include 'gabarit.php' ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Dosmoz form</title>
+	<link rel="stylesheet" type="text/css" href="stylesheet/global.css">
+</head>
+<body>
+	
+		<div class="container">
+			  <div class="circle embossed">
+			    <p class="embossedtext">Dosmoz</p>
+			  </div>
+			
+			  <div class="content embossed">
+			    <p class="embossedtext">
+			    Dosmoz est un projet r&Eacutealis&Eacute par une des &Eacutequipes de Mohai &Agrave la demande de DomIsep. Une &Eacutequipe de 6 grands apprentis ing&Eacutenieurs a con&ccedilu ce produit.
+			  </div>
+</div>
+		<footer><?php include 'Vue/footer.php';?></footer>
 </body>
-<footer><?php include '../Vue/footer.php';?></footer>
+
 </html>
