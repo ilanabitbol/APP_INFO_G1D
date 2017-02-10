af8eaf42f6c1b0ac6e0d779cb23e91156f347be7
cradolanirina <clement.radolanirina@gmail.com>
2017-02-10 14:18:50 +0100

    changement pour la connexion à la base

--- ebauche_ilan/modele/Connexion_Base.class.php
+++ ebauche_ilan/modele/Connexion_Base.class.php
@@ -3,8 +3,8 @@ class Connexion_Base{
 	private $db;
 	//Constructeur
 	public function __construct(){
-		//$this->db = new PDO('mysql:host=mysql.hostinger.fr;dbname=u691514939_abyve;charset=utf8', 'u691514939_eqyde', 'Cr19952002.');
-		$this->db = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE;charset=utf8', 'root', 'root');
+		$this->db = new PDO('mysql:host=mysql.hostinger.fr;dbname=u691514939_abyve;charset=utf8', 'u691514939_eqyde', 'Cr19952002.');
+		//$this->db = new PDO('mysql:host=localhost;dbname=APP_G1D_BASE;charset=utf8', 'root', 'root');
 	}
 	//Getter 
 	public function getDb(){
