2a4015c5a8749ec8a0ea3fbbda7d1243ed6b599c
fosfor12345 <enkalus@gmail.com>
2017-02-10 11:15:18 +0100

    index

--- ebauche_ilan/index.php
+++ ebauche_ilan/index.php
@@ -8,16 +8,41 @@
 </head>
 <body>
 	
-		<div class="container">
-			  <div class="circle embossed">
-			    <p class="embossedtext">Dosmoz</p>
-			  </div>
-			
-			  <div class="content embossed">
-			    <p class="embossedtext">
-			    Dosmoz est un projet r&Eacutealis&Eacute par une des &Eacutequipes de Mohai &Agrave la demande de DomIsep. Une &Eacutequipe de 6 grands apprentis ing&Eacutenieurs a con&ccedilu ce produit.
-			  </div>
-</div>
+		<div class="perspective">
+	  
+	  		<ul class="wrapper">
+	    
+			    <li class="top_3px"><section class="produit">Thermostat</section>
+			    		<section class="description"><p>Ce thermostat connect&#233 vous permet de connaitre la temp&#233rature de votre domicile ou que vous soyez !</p>
+			   			 <a  href='shop.php'><button class="boutonbleu" id="vitrine">Acheter</button></a>
+			   			</section>
+			    </li>
+			    <li class="top_3px"><section class="produit">An&#233mom&#232tre</section>
+			    		<section class="description"><p>Surveillez les vents et rep&#233rez le meilleur moment de la journ&#233e pour aller faire du kite-surf avec cet an&#233mom&#232tre !</p>
+			   			 <a  href='shop.php'><button class="boutonbleu" id="vitrine">Acheter</button></a>
+			   			</section>  
+			   	</li>
+			    <li class="top_3px"><section class="produit">Cam&#233ra</section>
+			    	<section class="description"><p>Cette cam&#233ra derni&#232re g&#233n&#233ration avec vision de nuit est la garante de la s&#233curit&#233 de votre maison pendant vos vacances !</p>
+			   			 <a  href='shop.php'><button class="boutonbleu" id="vitrine">Acheter</button></a>
+			   			</section>   
+			   	</li>
+			    <li class="top_3px"><section class="produit">Pluviom&#232tre</section>
+			    <section class="description"><p>Vous ne savez pas quand d&#233clencher l'arrosage automatique ? Fiez vous a ce pluviom&#232tre, il vous donnera la r&#233ponse !</p>
+			   			 <a  href='shop.php'><button class="boutonbleu" id="vitrine">Acheter</button></a>
+			   			</section> 
+			   	</li>
+			    <li class="top_3px"><section class="produit">Station m&#233t&#233o</section>
+			   <section class="description"><p>Un concentr&#233 de technologie a placer a l'int&#233rieur de votre domicile pour avoir une vision globale de votre atmosph&#232re !</p>
+			   			 <a  href='shop.php'><button class="boutonbleu" id="vitrine">Acheter</button></a>
+			   			</section>  
+			   	</li>
+	    
+	 		 </ul>
+
+		</div>	
+		 	<script type="text/javascript" src="http://code.jquery.com/jquery.js"></script>
+			<script type="text/javascript" src="../stylesheet/accueil.js"></script>
 		<footer><?php include 'Vue/footer.php';?></footer>
 </body>
 
